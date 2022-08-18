bfeb0e65c13ffd40e71960a916c2383b5069955d
Uzielmf <Uziel.munozf@gmail.com>
2022-08-18 14:47:11 -0500

    se agregaran los documentos basicos

---
+++ main.js
@@ -0,0 +1,70 @@
+
+let lenguaje;
+let lenguaje2;
+let mesnaje;
+
+
+
+
+
+/*
+
+//resultado = total.toString();
+//resultado=mensaje.toLowerCase();
+//resultado=mensaje.toUpperCase();
+
+resultado=mensaje.concat(mensjae2);
+
+console.log(resultado);
+
+//repeat (para reperie una sentencia determinada veces)
+//resultado = mensaje.repeat(2)
+
+//REPLACE para poder remplax=zarlo, el primer parametro es lo que se uiere cambiar y el segundo a que se queire cambiar
+//resultado=mensaje.replace("Javascript" , "a reventar ocicos")
+
+//SLICE cortara hasta la posisicion que se selecciona en este caso 6, y el segundo para que parte se dejara
+//resultado = mensaje.slice(6, 10);
+
+//SPLIT
+//resultado = mensaje.split();
+
+//TRIM sirve para eliminar los espacios en blanco que hay en una cadena, al inicio y al final de ella.
+//resultado=mensaje.trim();
+
+
+//console.log(resultado);
+
+/*
+let texton= mensaje.indexOf('Javascript')
+
+
+console.log(texton);
+console.log(resultado);
+//resultado = mensaje.startsWith('Ja');
+
+/*let mensaje = "Estoy aprendiendo Javascript y estoy aprendidendo muncho";
+
+let resultado;
+
+//match
+//resultado = mensaje.match(/aprendiendo/g);
+//console.log(resultado);
+
+//substr, te retorna el pedaze de texto que estas buscando y luego con este lo guardas
+//resultado = mensaje.substr(2,14);
+//console.log(resultado);
+//CUENTA APARTIR DEL PRIMER PARAMETRO
+
+
+//substring
+resultado = mensaje.substring(6,17);
+
+//CUENTA DESDE EL INICIO DE LA CADENA Y EL PRIMER PARAMETRO ES DONDE INICIARA A SELECCIONAR
+
+
+
+//charArt
+resultado = mensaje.charAt(3);
+*/
+
